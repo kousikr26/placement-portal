3bc301e9cca244947368dd24c9f55091e66ae029
kousikr26 <kousik18@iitg.ac.in>
2020-05-05 16:27:37 +0530

    Added charts

--- home/views.py
+++ home/views.py
@@ -5,11 +5,48 @@ from django.template import RequestContext
 from django.template.context_processors import csrf
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-
+import json
 
 def index(request):
+	branches = ["CSE", "MNC", "ECE", "EEE", "ME", "CE", "CL", "EP", "CST", "BT", "Physics", "Chemistry", "Mathematics", "Design", "Others"]
+	
+	btech_percent_placed = json.dumps({"Placed": len(Student.objects.filter(
+		programs='B.Tech').filter(placed=True)), "Not placed": len(Student.objects.filter(programs='B.Tech').filter(placed=False))})
+	mtech_percent_placed = json.dumps({"Placed": len(Student.objects.filter(
+		programs='M.Tech').filter(placed=True)), "Not placed": len(Student.objects.filter(programs='M.Tech').filter(placed=False))})
+	btech_branchwise_placements=[]
+	mtech_branchwise_placements = []
+	for bch in branches:
+		tmp={}
+		tmp["group"]=bch
+		num = len(Student.objects.filter(programs='B.Tech').filter(
+		    branch__branchName=bch).filter(placed=True))
+		den=(len(Student.objects.filter(programs='B.Tech').filter(branch__branchName=bch) ) )
+		if(den==0):
+			continue
+		tmp["value"] = (num/den)*100
+		
+		btech_branchwise_placements.append(tmp)
+	for bch in branches:
+		tmp = {}
+		tmp["group"] = bch
+		num = len(Student.objects.filter(programs='M.Tech').filter(
+		    branch__branchName=bch).filter(placed=True))
+		den = (len(Student.objects.filter(
+			programs='M.Tech').filter(branch__branchName=bch)))
+		if(den == 0):
+			continue
+		tmp["value"] = (num/den)*100
 
-	return render(request,"home/stats.html")
+		mtech_branchwise_placements.append(tmp)
+	context = {"btech_percent_placed": btech_percent_placed, 
+			"mtech_percent_placed": mtech_percent_placed,
+            "btech_branchwise_placements": json.dumps(btech_branchwise_placements), 
+			"mtech_branchwise_placements": json.dumps(mtech_branchwise_placements),
+			}
+
+	print(context)
+	return render(request, "home/stats.html",context )
 
 # @login_required
 # def charts(request):
---
+++ static/home/bar.js
@@ -0,0 +1,64 @@
+
+// set the dimensions and margins of the graph
+var margin = { top: 20, right: 20, bottom: 20, left: 20 },
+    width = 400 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#bar")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+// Initialize the X axis
+var x = d3.scaleBand()
+    .range([0, width])
+    .padding(0.2);
+var xAxis = svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+
+// Initialize the Y axis
+var y = d3.scaleLinear()
+    .range([height, 0]);
+var yAxis = svg.append("g")
+    .attr("class", "myYaxis")
+
+
+// A function that create / update the plot for a given variable:
+function render2(data) {
+
+    // Update the X axis
+    x.domain(data.map(function (d) { return d.group; }))
+    xAxis.call(d3.axisBottom(x))
+
+    // Update the Y axis
+    y.domain([0, d3.max(data, function (d) { return d.value })]);
+    yAxis.transition().duration(1000).call(d3.axisLeft(y));
+
+    // Create the u variable
+    var u = svg.selectAll("rect")
+        .data(data)
+
+    u
+        .enter()
+        .append("rect") // Add a new rect for each new elements
+        .merge(u) // get the already existing elements as well
+        .transition() // and apply changes to all of them
+        .duration(1000)
+        .attr("x", function (d) { return x(d.group); })
+        .attr("y", function (d) { return y(d.value); })
+        .attr("width", x.bandwidth())
+        .attr("height", function (d) { return height - y(d.value); })
+        .attr("fill", "#69b3a2")
+
+    // If less group in the new dataset, I delete the ones not in use anymore
+    u
+        .exit()
+        .remove()
+}
+
+// Initialize the plot with the first dataset
+render2(data3)
