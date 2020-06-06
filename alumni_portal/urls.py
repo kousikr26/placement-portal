dd9d899fdef1ab60a916837a2bce83380c2dbf08
Sudesh1122 <sudesh18@iitg.ac.in>
2020-06-06 20:40:47 +0530

    add post's cards and detail view for the posts

--- alumni_portal/urls.py
+++ alumni_portal/urls.py
@@ -4,10 +4,11 @@ from django.conf.urls.static import static
 
 from .views import *
 
-app_name = "blog"
+app_name = "alumni_portal"
 urlpatterns = [
     path('',home, name='home'),
     path('new',CreatePost.as_view(),name="create_post"),
+    path('post/<str:slug>',PostDetailView.as_view(),name='post_detail')
 ]
 
 if settings.DEBUG:
--- alumni_portal/views.py
+++ alumni_portal/views.py
@@ -3,11 +3,23 @@ from django.views import generic
 from django.urls import reverse_lazy
 
 from .forms import *
+from .models import *
 # Create your views here.
 def home(request):
-    return render(request,"blog/index.html")
+    # only published posts will be shown
+    posts = Post.objects.all().filter(status='published');
+    # # truncatewords
+    # for i in range(len(posts)):
+    #     l = len(posts[i].body)
+    #     l = min(l,300)
+    #     posts[i].body = posts[i].body[:l]
+    context = {'posts':posts}
+    return render(request,"alumni_portal/index.html",context)
 
 class CreatePost(generic.CreateView):
     form_class = PostForm
-    template_name = 'blog/create_post.html'
-    success_url = reverse_lazy('blog:home')
+    template_name = 'alumni_portal/create_post.html'
+    success_url = reverse_lazy('alumni_portal:home')
+
+class PostDetailView(generic.DetailView):
+    model = Post
