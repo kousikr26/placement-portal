2ba6ef95dda3f95315bcb9fc643a8755bdc14525
Sudesh1122 <sudesh18@iitg.ac.in>
2020-07-22 01:43:49 +0530

    add company filter using url query

--- home/views.py
+++ home/views.py
@@ -7,77 +7,83 @@ from django.http import HttpResponseRedirect,JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.db.models.functions import Lower
 from django.template.loader import render_to_string
+from django.utils.text import slugify
 
 import json
 
 def index(request):
-	branches = ["CSE", "MNC", "ECE", "EEE", "ME", "CE", "CL", "EP", "CST", "BT", "Physics", "Chemistry", "Mathematics", "Design", "Others"]
+    branches = ["CSE", "MNC", "ECE", "EEE", "ME", "CE", "CL", "EP", "CST", "BT", "Physics", "Chemistry", "Mathematics", "Design", "Others"]
 
-	btech_percent_placed = json.dumps({"Placed": len(Student.objects.filter(
-		programs='B.Tech').filter(placed=True)), "Not placed": len(Student.objects.filter(programs='B.Tech').filter(placed=False))})
-	mtech_percent_placed = json.dumps({"Placed": len(Student.objects.filter(
-		programs='M.Tech').filter(placed=True)), "Not placed": len(Student.objects.filter(programs='M.Tech').filter(placed=False))})
-	btech_all=Student.objects.filter(programs='B.Tech').filter(placed=True)
-	comp_counts={}
+    btech_percent_placed = json.dumps({"Placed": len(Student.objects.filter(
+        programs='B.Tech').filter(placed=True)), "Not placed": len(Student.objects.filter(programs='B.Tech').filter(placed=False))})
+    mtech_percent_placed = json.dumps({"Placed": len(Student.objects.filter(
+        programs='M.Tech').filter(placed=True)), "Not placed": len(Student.objects.filter(programs='M.Tech').filter(placed=False))})
+    btech_all=Student.objects.filter(programs='B.Tech').filter(placed=True)
+    comp_counts={}
 
-	for i in btech_all:
-		if(i.company in comp_counts):
-			comp_counts[i.company]+=1
-		else:
-			comp_counts[i.company]=1
-	print(comp_counts)
-	comp_count_lis=[]
-	for i in comp_counts:
-		tmp={}
-		if(i==""):
-			continue
-		tmp["tag"]=i
-		tmp["weight"]=comp_counts[i]
-		comp_count_lis.append(tmp)
-	btech_branchwise_placements=[]
-	mtech_branchwise_placements = []
-	cse_wc=[]
-	for bch in branches:
-		tmp={}
-		tmp["group"]=bch
-		num = len(Student.objects.filter(programs='B.Tech').filter(
-		    branch__branchName=bch).filter(placed=True))
-		den=(len(Student.objects.filter(programs='B.Tech').filter(branch__branchName=bch) ) )
-		if(den==0):
-			continue
-		tmp["value"] = (num/den)*100
+    for i in btech_all:
+        if(i.company in comp_counts):
+            comp_counts[i.company]+=1
+        else:
+            comp_counts[i.company]=1
+    print(comp_counts)
+    comp_count_lis=[]
+    for i in comp_counts:
+        tmp={}
+        if(i==""):
+            continue
+        tmp["tag"]=i
+        tmp["weight"]=comp_counts[i]
+        comp_count_lis.append(tmp)
+    btech_branchwise_placements=[]
+    mtech_branchwise_placements = []
+    cse_wc=[]
+    for bch in branches:
+        tmp={}
+        tmp["group"]=bch
+        num = len(Student.objects.filter(programs='B.Tech').filter(
+            branch__branchName=bch).filter(placed=True))
+        den=(len(Student.objects.filter(programs='B.Tech').filter(branch__branchName=bch) ) )
+        if(den==0):
+            continue
+        tmp["value"] = (num/den)*100
 
-		btech_branchwise_placements.append(tmp)
-	for bch in branches:
-		tmp = {}
-		tmp["group"] = bch
-		num = len(Student.objects.filter(programs='M.Tech').filter(
-		    branch__branchName=bch).filter(placed=True))
-		den = (len(Student.objects.filter(
-			programs='M.Tech').filter(branch__branchName=bch)))
-		if(den == 0):
-			continue
-		tmp["value"] = (num/den)*100
+        btech_branchwise_placements.append(tmp)
+    for bch in branches:
+        tmp = {}
+        tmp["group"] = bch
+        num = len(Student.objects.filter(programs='M.Tech').filter(
+            branch__branchName=bch).filter(placed=True))
+        den = (len(Student.objects.filter(
+            programs='M.Tech').filter(branch__branchName=bch)))
+        if(den == 0):
+            continue
+        tmp["value"] = (num/den)*100
 
-		mtech_branchwise_placements.append(tmp)
-	context = {"btech_percent_placed": btech_percent_placed,
-			"mtech_percent_placed": mtech_percent_placed,
+        mtech_branchwise_placements.append(tmp)
+    context = {"btech_percent_placed": btech_percent_placed,
+            "mtech_percent_placed": mtech_percent_placed,
             "btech_branchwise_placements": json.dumps(btech_branchwise_placements),
-			"mtech_branchwise_placements": json.dumps(mtech_branchwise_placements),
-			"company_count":comp_count_lis,
-			}
+            "mtech_branchwise_placements": json.dumps(mtech_branchwise_placements),
+            "company_count":comp_count_lis,
+            }
 
-	# print(context)
-	return render(request, "home/stats.html",context )
+    # print(context)
+    return render(request, "home/stats.html",context )
 
 ################################################################################
 # function to render the table
 
 def get_table(request):
-	students  = Student.objects.filter(placed=True)
-	branches = Branch.objects.all()
-	context = {'students':students,'branches':branches}
-	return render(request,'home/table_home.html',context)
+    company = request.GET.get('company')
+    if company:
+        students  = Student.objects.filter(company=company)
+    else:
+        students  = Student.objects.filter(placed=True)
+
+    branches = Branch.objects.all()
+    context = {'students':students,'branches':branches}
+    return render(request,'home/table_home.html',context)
 
 ################################################################################
 
@@ -88,12 +94,16 @@ def ajax_table_filter(request):
         branch = request.GET.get('branch')
         program = request.GET.get('program')
         sortid = request.GET.get('sortid')
+        company = request.GET.get('company')
         students = Student.objects.filter(placed=True)
+        if company:
+            students= students.filter(company=company)
         if branch!='all':
             branch = Branch.objects.get(branchName=branch)
             students = students.filter(branch=branch)
         if program!='all':
             students = students.filter(programs=program)
+
         students = students.order_by(Lower(sortid))
         data['table_data_html'] = render_to_string('home/table_data.html',{'students':students})
         data['success'] = True
