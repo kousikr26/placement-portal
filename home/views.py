57df8b0a958c0992809ae0d81bb7143e36d164c5
kousikr26 <kousik18@iitg.ac.in>
2020-07-22 04:05:01 +0530

    few changes and merge

--- home/views.py
+++ home/views.py
@@ -88,62 +88,7 @@ def index(request):
 			"company_count":comp_count_lis,
 			"company_count_mtech":comp_count_lis_mtech
 			}
-	branches = ["CSE", "MNC", "ECE", "EEE", "ME", "CE", "CL", "EP", "CST", "BT", "Physics", "Chemistry", "Mathematics", "Design", "Others"]
-
-	btech_percent_placed = json.dumps({"Placed": len(Student.objects.filter(
-		programs='B.Tech').filter(placed=True)), "Not placed": len(Student.objects.filter(programs='B.Tech').filter(placed=False))})
-	mtech_percent_placed = json.dumps({"Placed": len(Student.objects.filter(
-		programs='M.Tech').filter(placed=True)), "Not placed": len(Student.objects.filter(programs='M.Tech').filter(placed=False))})
-	btech_all=Student.objects.filter(programs='B.Tech').filter(placed=True)
-	comp_counts={}
-
-	for i in btech_all:
-		if(i.company in comp_counts):
-			comp_counts[i.company]+=1
-		else:
-			comp_counts[i.company]=1
-	print(comp_counts)
-	comp_count_lis=[]
-	for i in comp_counts:
-		tmp={}
-		if(i==""):
-			continue
-		tmp["tag"]=i
-		tmp["weight"]=comp_counts[i]
-		comp_count_lis.append(tmp)
-	btech_branchwise_placements=[]
-	mtech_branchwise_placements = []
-	cse_wc=[]
-	for bch in branches:
-		tmp={}
-		tmp["group"]=bch
-		num = len(Student.objects.filter(programs='B.Tech').filter(
-			branch__branchName=bch).filter(placed=True))
-		den=(len(Student.objects.filter(programs='B.Tech').filter(branch__branchName=bch) ) )
-		if(den==0):
-			continue
-		tmp["value"] = (num/den)*100
-
-		btech_branchwise_placements.append(tmp)
-	for bch in branches:
-		tmp = {}
-		tmp["group"] = bch
-		num = len(Student.objects.filter(programs='M.Tech').filter(
-			branch__branchName=bch).filter(placed=True))
-		den = (len(Student.objects.filter(
-			programs='M.Tech').filter(branch__branchName=bch)))
-		if(den == 0):
-			continue
-		tmp["value"] = (num/den)*100
-
-		mtech_branchwise_placements.append(tmp)
-	context = {"btech_percent_placed": btech_percent_placed,
-			"mtech_percent_placed": mtech_percent_placed,
-			"btech_branchwise_placements": json.dumps(btech_branchwise_placements),
-			"mtech_branchwise_placements": json.dumps(mtech_branchwise_placements),
-			"company_count":comp_count_lis,
-			}
-
+	
 	# print(context)
 	return render(request, "home/stats.html",context )
 
